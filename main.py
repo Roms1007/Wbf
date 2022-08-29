7941415fc7959daa232fc8248cd6c0d1aedc93b8
ml704457 <mandelbaumleon@gmail.com>
2022-08-29 16:42:24 +0200

    premier commit de léon

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+liste = {"animaux": ["lion", "dauphin", "chameau", "léopard"],
+         "humain": ["Stephane", "Romain", "Kevin", "Alexis", "Toto"], "objet": ["PC", "Stylo", "cahier"]}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def remplacer(key, input, key1, input1):
+    liste.pop(key[input])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def remplacermenu():
+    flag = True
+    while flag :
+        print("Que voulez rajouter ?")
+        print("1. Un animaux")
+        print("2. Un humain")
+        print("3. Un objet")
+        choix = input()
+        match choix:
+            case "1":
+                print("Quel animal voulez-vous remplacer ?")
+                animal = input()
+                for i in range (len(liste)):
+                    if
+                        remplacer(animal, choix)
+                    else:
+            case "2":
+                print("Quel humain souhaitez-vous remplacer ?")
+            case "3":
+                print("Quel objet voulez-vous remplacer ?")
+            case _:
+                print("Erreur : commande incorrecte")
+
+print("lol")
+
