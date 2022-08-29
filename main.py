f417ffe93c62616d24640ad24a3d62251fc876c4
Senuoy7 <younesbellakhal@gmail.com>
2022-08-29 16:43:39 +0200

    premier commit de younes

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def supprimer(dico):
+    print("Quel élément du dictionnaire souhaitez vous supprimer: ")
 
+def afficher(dico):
+    print(dico)
+    return menu()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def menu():
+    dico = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1966,
+}
+    flag = True
+    while flag == True:
+        print("Bienvenue dans le dictionnaire, que souhaitez vous faire : ")
 
+        print("1 : Ajouter un élément dans le dictionnaire")
+        print("2 : Supprimer un élément du dictionnaire")
+        print("3 : Modifier un élément du dictionnaire")
+        print("4 : Remplacer un élément du dictionaire")
+        print("5 : Afficher dictionnaire")
+        print("Q : Quitter")
 
-# Press the green button in the gutter to run the script.
+        choix = input()
+
+        match choix:
+            case "1":
+                print("hello")
+            case "2":
+                supprimer(dico)
+            case "3":
+                print("hello")
+            case "4":
+                print("hello")
+            case "5":
+                afficher(dico)
+            case "Q":
+                return
+            case _:
+                print("erreur, entrez une valeur entre 1 et 5, ou entrez Q pour quitter")
+                return menu()
+
+def main():
+    menu()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
